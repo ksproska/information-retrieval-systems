efd707ee0e7327ef11cbbcba430bd7b4710d5003
Kamila Sproska <kamila.sproska@gmail.com>
2024-03-21 14:18:50 +0100

    script to test query time added

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ elasticsearch - Official Image | Docker Hub
 - https://typesense.org/learn/elasticsearch-bulk-import-json/
 - https://onexlab-io.medium.com/elasticsearch-bulk-insert-json-data-322f97115d8d
 - https://www.youtube.com/watch?v=H1dyC9iuTHA&t=1s
+
+- https://gist.github.com/zsprackett/8546403
+- https://logz.io/blog/elasticsearch-mapping/
+- https://coralogix.com/blog/42-elasticsearch-query-examples-hands-on-tutorial/
---
+++ test_queries.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for FILE in "./query_data"/*
+do
+    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    if [ -f "$FILE" ]; then
+        filename=$(basename "$FILE")
+        elapsed_time=$(time (curl -s -X GET 'localhost:9200/movies/_search?pretty' -H 'Content-Type: application/json' -d "@$FILE" --output "./query_response/$filename") 2>&1)
+        real_time=$(echo "$elapsed_time" | grep "real" | tr ',' '.' | awk '{print $NF}')
+        echo "$timestamp,$filename,$real_time" >> "test_time_elapsed_for_queries.csv"
+    fi
+done
