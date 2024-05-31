2e5e3c6e9ed53c5d0257bab8d2ba30d173017587
Numpek2k <theinqu@gmail.com>
2024-05-31 12:28:20 +0200

    main-page, added bootstrap icons and logo

--- find-my-rock/src/app/components/main-page/main-page.component.ts
+++ find-my-rock/src/app/components/main-page/main-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgOptimizedImage
   ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
