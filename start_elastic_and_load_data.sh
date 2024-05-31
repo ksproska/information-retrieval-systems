53fd5f586f278fab346510f8ea02acd79f4c7cbf
Kamila Sproska <kamila.sproska@gmail.com>
2024-05-31 19:29:29 +0200

    added script for elastic setup + data loading

---
+++ start_elastic_and_load_data.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "elasticsearch docker container starting..."
+docker compose up -d elasticsearch
+
+echo "creating index..."
+sleep 15
+until curl -X PUT -s --output /dev/null --fail http://localhost:9200/routes; do
+    echo "Trying again..."
+    sleep 5
+done
+
+echo "index created, proceeding..."
+echo "loading data..."
+
+for file in ./elasticsearch_data/*; do
+    curl -s -o /dev/null -w "${file}: %{http_code}\n" -H "Content-Type: application/x-ndjson" \
+        --data-binary "@${file}" http://localhost:9200/routes/_bulk
+done
+
+echo "data loaded"
+echo "setup complete"
