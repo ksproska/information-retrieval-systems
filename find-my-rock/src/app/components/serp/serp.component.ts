f6e9379bbbd7829249e314982c80698fd5d22dd3
Kamila Sproska <kamila.sproska@gmail.com>
2024-06-01 09:37:51 +0200

    connecting sorting mechanizm from frontend to backend

--- find-my-rock/src/app/components/serp/serp.component.ts
+++ find-my-rock/src/app/components/serp/serp.component.ts
@@ -1,8 +1,8 @@
-import {Component} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {FormsModule} from "@angular/forms";
-import {ElasticsearchService} from "../../services/elasticsearch.service";
-import {JsonPipe, NgIf} from "@angular/common";
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from "@angular/forms";
+import { ElasticsearchService } from "../../services/elasticsearch.service";
+import { JsonPipe, NgIf } from "@angular/common";
 
 @Component({
   selector: 'app-serp',
@@ -17,20 +17,25 @@ import {JsonPipe, NgIf} from "@angular/common";
 })
 export class SerpComponent {
   query: string = "";
+  selectedSort: string = "none";
   searchResults: any;
   errorMessage: string | null = null;
 
   constructor(private route: ActivatedRoute, private elasticsearchService: ElasticsearchService) {
     this.route.queryParams.subscribe(params => {
-      this.query = params['query'];
-      this.elasticsearchService.searchInAllFieldsWithFilters(this.query).subscribe({
-        next: (response) => {
-          this.searchResults = response.hits.hits;
-        },
-        error: (error) => {
-          this.errorMessage = 'Search error: ' + error.message;
-        }
-      });
+      this.query = params['query'] || "";
+      this.updateSearch();
+    });
+  }
+
+  updateSearch() {
+    this.elasticsearchService.searchInAllFieldsWithFilters(this.query, [], '3rd', 'V?', 1, 10, '', this.selectedSort).subscribe({
+      next: (response) => {
+        this.searchResults = response.hits.hits;
+      },
+      error: (error) => {
+        this.errorMessage = 'Search error: ' + error.message;
+      }
     });
   }
 }
